6015f5c5d6f29c1c2347064752f6192263c2b4f0
Germán Vanni <germanvanni1@gmail.com>
2024-05-05 21:55:19 -0300

    initial

---
+++ src/test/java/dan/ms/tp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.ms.tp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
